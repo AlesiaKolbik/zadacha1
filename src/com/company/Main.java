424f20400e942a1ddc0fdc32a5a6c9bdce391808
alesya.kolbik <moyaalesia@mail.ru>
2017-10-13 16:58:47 +0300

    условия закроет ли круг с радиусом r четырежугольное отверстие со сторонами а и в.

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = (int)3;
+	int b= (int)4;
+	int r= (int)3;
+	int c= (int)a+b;
+	if (c>=r){
+	    System.out.println("Закроет.");
+    }
+    else {
+	    System.out.println("Не закроет.");
+
+    }
+    }
+}
